7a71aeb5b036bcb922194cf3121ba7373442254e
deziq <deziq.develop@gmail.com>
2019-11-04 17:35:39 +0100

    fix bug static file

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,6 +9,7 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    #num_genres = Genre.objects.all.count()
     
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -16,11 +17,15 @@ def index(request):
     # The 'all()' is implied by default.    
     num_authors = Author.objects.count()
     
+    num_book_articolo_il = Book.objects.filter(title__exact='il').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_articoli_il':num_book_articolo_il,
+        #'num_genres':num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
