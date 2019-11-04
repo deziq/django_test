29ad022a465aaa8da5e0ff633a682fe585572846
m0rph3u5-zip <cia.waglio@yahoo.com>
2019-11-05 00:24:18 +0100

    fix view

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
-# Used to generate URLs by reversing the URL patterns
-from django.urls import reverse
-
-# Required for unique book instances 
-import uuid
-
-
+# Create your models here.
 class Genre(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Inserisci il genere del libro (es Scienza, Commedia, ecc)')
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
     
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
+
+
+
+from django.urls import reverse 
+# Used to generate URLs by reversing the URL patterns
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -47,6 +46,11 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'
 
 
+
+
+import uuid
+# Required for unique book instances
+
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
@@ -75,6 +79,8 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+
 
 
 class Author(models.Model):
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -7,18 +7,17 @@ def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
-    num_books               = Book.objects.all().count()
-    num_instances           = BookInstance.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    #num_genres = Genre.objects.all.count()
     
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     
     # The 'all()' is implied by default.    
-    num_authors             = Author.objects.count()
-       
-    num_book_articolo_il    = Book.objects.filter(title__icontains='il').count()
-    num_genre               = Genre.objects.filter(name__icontains='t').count()
-    demo                    = 0
+    num_authors = Author.objects.count()
+    
+    num_book_articolo_il = Book.objects.filter(title__exact='il').count()
 
     context = {
         'num_books': num_books,
@@ -26,8 +25,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_articoli_il':num_book_articolo_il,
-        'num_genre':num_genre,
-        'demo':demo,
+        #'num_genres':num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
