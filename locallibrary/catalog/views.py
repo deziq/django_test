fadcb2d285cc1c14e8699dd2d16d0b74b0d0c2ed
deziq <deziq.develop@gmail.com>
2019-11-06 10:41:44 +0100

    fixed bug

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Genre
@@ -28,5 +29,28 @@ def index(request):
         'num_genres':num_genres,
     }
 
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    
+
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
