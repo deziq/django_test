0674458363d0acba62b2de380855d5ba3defacf5
deziq <deziq.develop@gmail.com>
2019-11-08 12:59:43 +0100

    user area

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,13 +4,10 @@ from . import views
 urlpatterns = [
     #Index url
     path('', views.index, name='index'),
-    
     #book url 
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
-    
     # Author url
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
-
 ]
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     # Add URL maps to redirect the base URL to our application
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     #Add Django site authentication urls (for login, logout, password management)
-    path('account/', include('django.contrib.auth.urls')),
+    path('utente/', include('django.contrib.auth.urls')),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
